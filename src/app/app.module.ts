eecd93f19cfb8b2c92babb0d42fefe9b0f0016f7
iMrLopez <marny.lopez@outlook.com>
2020-06-30 19:21:36 -0600

    creamos los componentes vacios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListaComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
